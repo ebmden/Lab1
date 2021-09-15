9026fd85c666724ccd7daee06639b5e4c01c955e
grallucas <90278798+grallucas@users.noreply.github.com>
2021-09-15 08:05:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from showbits import shorthand, bits
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print('question 7:')
+shorthand(b'2 Faced') #output: 32 20 46 61 63 65 64
+#check question 1
 
+print('question 9:')
+bits(104) #output: 1101000
+shorthand(104) #output: 68
+#check question 3
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print('question 11:')
+bits(b'\xbeef') #output: 10111110 01100101 01100110
+shorthand(b'\xbeef') #output: be 65 66
+#check question 5
 
+#print('question 13:')
+#13 a) type(i) = int
+#13 b) type(int) = type
+#13 c) INT is a type that stores a whole number, and TYPE is a type that stores a type. They're different because they contextualize data differently; 0x0F could be 15 or it could represent the specific type of a variable.
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#print('question 15:')
+# 15 a) 01 04 01 05 01 0A 01 07 00
+# 15 b)
+#message = b'\x01\x04\x01\x05\x01\x0A\x01\x07\x00'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#print('question 17:')
